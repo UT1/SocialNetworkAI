58e835fe0deef4fb1c933ee4dca068dcc7a6e409
Utkarsh Srivastava <UT1@users.noreply.github.com>
2017-11-16 19:56:39 +0530

    Delete Old Files

--- Wit_Speech_to_text.py
+++
@@ -1,8 +0,0 @@
-from wit import Wit
-from Wit_Credentials import *
-client=Wit(Wit_token);
-
-resp = None
-with open('output.wav', 'rb') as f:
-  resp = client.speech(f, None, {'Content-Type': 'audio/wav'})
-print('Yay, got Wit.ai response: ' + str(resp))
